c6089e5839f6f9e3b4c26418c72eb4fa72f8e120
bramvanlaere <r0625771@student.thomasmore.be>
2017-04-17 20:22:59 +0200

    Create indextest.php

---
+++ indextest.php
@@ -0,0 +1,18 @@
+@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IMDterest</title>
+</head>
+<body>
+    <h1>IMDterest</h1>
+
+    </div>
+        <a href="includes/logout.inc.php">Log out</a>
+    </div>
+</body>
+</html> 
